4823e21afee1664316c7ce0b39dc511cb3586138
tejaswinireddybandi <balireddytejaswini@gmail.com>
2024-06-14 20:24:25 -0700

    Initial commit

---
+++ DockerHub/src/main/java/com/example/DockerHub/UserService.java
@@ -0,0 +1,16 @@
+package com.example.DockerHub;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
